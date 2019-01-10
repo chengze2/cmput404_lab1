6297e2b378034f4d9b4e2c0f64863662f9fa0e98
chengze2 <chengze2@ualberta.ca>
2019-01-09 18:10:18 -0700

    first commit

---
+++ lab1.py
@@ -0,0 +1,11 @@
+import requests
+
+print(requests.__version__)
+
+r = requests.get("http://www.google.com")
+print(r.status_code)
+print(r.text)
+#print(dir(r))
+
+a = requests.get()
+print(a.text)
